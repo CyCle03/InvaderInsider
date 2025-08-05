239d2ffa9cfdcc65c2240f182614803756609f1a
CyCle03 <cherrybear03@naver.com>
2025-08-05 15:18:46 +0900

    Fix: Enable card drag-and-drop within ScrollRect
    The standard ScrollRect component was consuming all drag events, preventing the CardInteractionHandler on child card icons from functioning.
    
    This commit introduces CustomScrollRect.cs, a script that inherits from ScrollRect but intelligently passes drag events down to other IDragHandler components (like cards) within its hierarchy. This allows users to drag cards from the hand panel while still allowing the panel to be scrolled by dragging the background.

---
+++ Assets/MyAssets/Scripts/UI/CustomScrollRect.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace InvaderInsider.UI
+{
+    public class CustomScrollRect : ScrollRect
+    {
+        private bool forbidDrag = false;
+
+        public override void OnBeginDrag(PointerEventData eventData)
+        {
+            // 드래그 대상이 IDragHandler를 가지고 있는지 확인합니다.
+            // 자기 자신(ScrollRect) 외의 다른 핸들러를 찾습니다.
+            var dragHandlers = eventData.pointerPress.GetComponents<IDragHandler>();
+            bool shouldPassEvent = false;
+            foreach (var handler in dragHandlers)
+            {
+                if ((MonoBehaviour)handler != this)
+                {
+                    shouldPassEvent = true;
+                    break;
+                }
+            }
+
+            if (shouldPassEvent)
+            {
+                forbidDrag = true;
+                // 이벤트를 다른 핸들러에게 전달합니다.
+                ExecuteEvents.Execute(eventData.pointerPress, eventData, ExecuteEvents.beginDragHandler);
+            }
+            else
+            {
+                forbidDrag = false;
+                base.OnBeginDrag(eventData);
+            }
+        }
+
+        public override void OnDrag(PointerEventData eventData)
+        {
+            if (forbidDrag)
+            {
+                ExecuteEvents.Execute(eventData.pointerPress, eventData, ExecuteEvents.dragHandler);
+            }
+            else
+            {
+                base.OnDrag(eventData);
+            }
+        }
+
+        public override void OnEndDrag(PointerEventData eventData)
+        {
+            if (forbidDrag)
+            {
+                ExecuteEvents.Execute(eventData.pointerPress, eventData, ExecuteEvents.endDragHandler);
+                forbidDrag = false;
+            }
+            else
+            {
+                base.OnEndDrag(eventData);
+            }
+        }
+    }
+}
